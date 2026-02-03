2d0fa40c901d2356491900a9c2d4342ce78a5285
its-mohitkumar-7 <mohitkumarkk0704th@gmail.com>
2026-02-03 22:10:28 +0530

    Implement ProcessRunner logic

--- src/main/java/dev/vdokit/core/ProcessRunner.java
+++ src/main/java/dev/vdokit/core/ProcessRunner.java
@@ -1,8 +1,15 @@
 package dev.vdokit.core;
 
+import java.util.List;
 
 public class ProcessRunner {
 	
+	public static void run(List<String> command) throws Exception {
+		ProcessBuilder processBuilder = new ProcessBuilder(command);
+		processBuilder.inheritIO();
+		Process process = processBuilder.start();
+		process.waitFor();
+	}
 	
 	
 }
