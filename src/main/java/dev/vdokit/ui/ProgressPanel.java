4cfb51ddbadf84fae1e565cb01a9be7a41ed400d
its-mohitkumar-7 <mohitkumarkk0704th@gmail.com>
2026-03-19 20:20:44 +0530

    Improve ProgressPanel UI layout and alignment

--- src/main/java/dev/vdokit/ui/ProgressPanel.java
+++ src/main/java/dev/vdokit/ui/ProgressPanel.java
@@ -3,21 +3,38 @@ package dev.vdokit.ui;
 import javax.swing.*;
 import java.awt.*;
 
+
 public class ProgressPanel extends JPanel {
+
 	
 	private JProgressBar progressBar;
 	private JLabel progressBarLabel;
 	
+	
 	public ProgressPanel(){
+	
+		
+		setLayout(new GridBagLayout());
+		setVisible(false);
+		setBackground(new Color(181, 181, 181));
+		
+		GridBagConstraints GBC = new GridBagConstraints();
+		
+		GBC.weightx = 1.0;
+		GBC.weighty = 1.0;
+		GBC.anchor = GridBagConstraints.CENTER;
+		GBC.fill = GridBagConstraints.NONE;
 		
 		progressBar = new JProgressBar();
-		progressBarLabel = new JLabel("Processing...");
-		
 		progressBar.setIndeterminate(false);
-		add(progressBar);
-		add(progressBarLabel);
-		setVisible(false);	
+		progressBar.setBackground(new Color(115, 114, 114));
+		progressBar.setForeground(new Color(230, 230, 235));
+		progressBar.setPreferredSize(new Dimension(900, 50));
+		add(progressBar, GBC);
 		
+		progressBarLabel = new JLabel("Processing...");
+		add(progressBarLabel);
+	
 	}
 	
 	public void showProgress(String message){
@@ -25,7 +42,7 @@ public class ProgressPanel extends JPanel {
 		progressBarLabel.setText(message);
 		progressBar.setIndeterminate(true);
 		progressBar.setVisible(true);
-		
+			
 	}
 	
 	
@@ -33,7 +50,7 @@ public class ProgressPanel extends JPanel {
 		
 		progressBar.setIndeterminate(false);
 		progressBar.setVisible(false);
-		
+			
 	}
 
 }
